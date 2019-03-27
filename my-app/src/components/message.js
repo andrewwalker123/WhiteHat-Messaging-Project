243e481b3cd213b0078034d8096855d8c84779da
JadenVidal <jaden.vidal@sky.uk>
2019-03-27 16:13:56 +0000

    fixed message displaying minutes with 2 digits

--- my-app/src/components/message.js
+++ my-app/src/components/message.js
@@ -6,8 +6,12 @@ class Message extends Component {
     let date = new Date(this.props.messageInfo.timePosted);
     let hours = date.getHours();
     let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
     return `${hours} : ${minutes}`
   };
+
 
     render() {
       return (
