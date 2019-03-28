bde28577c9b383f1159ce619d66424aee529af54
JadenVidal <42211121+JadenVidal@users.noreply.github.com>
2019-03-28 15:10:10 +0000

    Update channelCard.js

--- my-app/src/components/channelCard.js
+++ my-app/src/components/channelCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const ChannelCard = (props) => {
-    console.log(props)
     return (
         <div className='items-stretch center ma0 bg-light-blue mb5 w-50 tl-ns bg-animate br4 ba b--black-10'>
         <Link className='no-underline' to={{pathname: `/channel/${props.slug}`, data: props}}>
