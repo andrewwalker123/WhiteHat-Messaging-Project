b3e0b789444df54c03718b5232b2356b57a14393
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-28 11:35:16 +0000

    channel list looking fyne

--- my-app/src/components/channelCard.js
+++ my-app/src/components/channelCard.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const ChannelCard = (props) => {
+    console.log(props)
     return (
-        <Link to={{pathname: `/channel/:${props.channel.channelName}`, data: props}}>
-            <h2>{props.channel.channelName}</h2>
-            <img src={props.channel.channelIcon}></img>
-            <h1>{props.channel.programme.title}</h1>
-        </Link>  
-
+        <div className='items-stretch center ma0 bg-light-blue mb5 w-50 tl-ns bg-animate br4 ba b--black-10'>
+        <Link className='no-underline' to={{pathname: `/channel/:${props.channel.channelName}`, data: props}}>
+            <img src={props.channel.channelIcon} width='600' height='83' className='center'></img>
+            <h1 className='tc white pt3 pb3'>{props.channel.programme.title}</h1>
+        </Link>
+        </div>
     )
 }
 export default ChannelCard
