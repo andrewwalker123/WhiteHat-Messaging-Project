bac755b46646e8226d837124455b8404948449ed
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-28 10:35:53 +0000

    pull request changes

--- my-app/src/containers/channelPage.js
+++ my-app/src/containers/channelPage.js
@@ -4,15 +4,15 @@ import Body from '../components/Body'
 import { Link } from 'react-router-dom'
 
 const ChannelPage = (props) => {
-  console.log(props)
+  const { channelName, channelIcon, programme: {contentUrl, title, desc} } = props.location.data.channel
     return (
       <div className="bg-washed-blue">
-          <Header headerTitle={props.location.data.channel.channelName} headerImage={props.location.data.channel.channelIcon}/>
+          <Header headerTitle={channelName} headerImage={channelIcon}/>
          <Body>
-           <img src={props.location.data.channel.programme.contentUrl} height='50%' width='50%'></img>
-           <h1>{props.location.data.channel.programme.title}</h1>
-           <h2>{props.location.data.channel.programme.desc}</h2>
-           <Link to={{pathname: `/channel/:${props.location.data.channel.channelName}/chat`, data: props.location.data}}>
+           <img src={contentUrl} height='50%' width='50%'></img>
+           <h1>{title}</h1>
+           <h2>{desc}</h2>
+           <Link to={{pathname: `/channel/:${channelName}/chat`, data: props.location.data}}>
             <div>
               <h1>Join Chat</h1>
             </div>
