ee2870f0a09ba9880d30482f4dbbef36adc6395d
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-28 10:32:10 +0000

    passed correct props

--- my-app/src/containers/channelPage.js
+++ my-app/src/containers/channelPage.js
@@ -12,7 +12,7 @@ const ChannelPage = (props) => {
            <img src={props.location.data.channel.programme.contentUrl} height='50%' width='50%'></img>
            <h1>{props.location.data.channel.programme.title}</h1>
            <h2>{props.location.data.channel.programme.desc}</h2>
-           <Link to={{pathname: `/channel/:${props.location.data.channel.channelName}/chat`, data: props}}>
+           <Link to={{pathname: `/channel/:${props.location.data.channel.channelName}/chat`, data: props.location.data}}>
             <div>
               <h1>Join Chat</h1>
             </div>
