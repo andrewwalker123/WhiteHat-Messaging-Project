258d3bf7d24083f7f22d5fa9a6b6fa6c60d72b66
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-28 10:21:48 +0000

    done with no styling

--- my-app/src/containers/channelPage.js
+++ my-app/src/containers/channelPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header'
 import Body from '../components/Body'
+import { Link } from 'react-router-dom'
 
 const ChannelPage = (props) => {
   console.log(props)
@@ -8,8 +9,15 @@ const ChannelPage = (props) => {
       <div className="bg-washed-blue">
           <Header headerTitle={props.location.data.channel.channelName} headerImage={props.location.data.channel.channelIcon}/>
          <Body>
-           <h1>{props.location.data.channel.programme.description}</h1>  
+           <img src={props.location.data.channel.programme.contentUrl} height='50%' width='50%'></img>
+           <h1>{props.location.data.channel.programme.title}</h1>
+           <h2>{props.location.data.channel.programme.desc}</h2>
           </Body>
+          <Link to={{pathname: `/channel/:${props.location.data.channel.channelName}/chat`, data: props}}>
+            <div>
+              <h1>Join Chat</h1>
+            </div>
+          </Link>
       </div>
     )
 }
