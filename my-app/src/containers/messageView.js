21c9ecf3293137276153d76d771efdcd37ab1e00
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-28 15:28:28 +0000

    asd

--- my-app/src/containers/messageView.js
+++ my-app/src/containers/messageView.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import MessageList from "../components/messageList";
 
-class MessageView extends Component  {
+export default class MessageView extends Component  {
   render(){
     return(
       <MessageList username={this.props.username} />
     )
   }
 }
-
-export default MessageView;
