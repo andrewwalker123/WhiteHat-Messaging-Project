3804871750a730af18645299ddeca2432b88eee0
Anna Burley <anna.burley@sky.uk>
2019-03-29 13:52:22 +0000

    Removed padding from channel listings

--- my-app/src/components/channelCard.js
+++ my-app/src/components/channelCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const ChannelCard = (props) => {
     return (
-        <div className='justify-center center ma0 mb4 w-50 tl-ns bg-animate br4 ba b--black-10'>
+        <div className='justify-center center ma0 mb4 tl-ns bg-animate br4 ba b--black-10'>
         <Link className='no-underline' to={{pathname: `/channel/${props.slug}`, data: props}}>
             <img src={props.channel.channelIcon} width='600' height='83' className='center'/>
         </Link>
