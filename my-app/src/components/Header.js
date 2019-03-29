748ae7b5c1ece94a2b4120e9f0183c16c359b70f
Anna Burley <anna.burley@sky.uk>
2019-03-29 11:53:07 +0000

    changed home button

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import header from '../css/header.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import home from '../assets/home.png';
 
 const Header = (props) => {
     if (props.headerImage == null) {
     return (
         <header className="gradient white bg-blue w-100 sans-serif ma0 pa2 pl3 top-0 flex justify-center">
-            <h1 className='mr2'>{props.headerTitle}</h1>
-            <Link to={{pathname: `/channel`}} className='f5 no-underline ph3 pv2 mb2 dib tc br3 bn bg-animate bg-black-70 hover-bg-black white absolute right-0'>Home</Link>
+            <Link to={{pathname: `/channel`}} className='home-button white'><img src={home}/></Link>
+            <h1 className='title-centered mr2'>{props.headerTitle}</h1>
         </header>
     )
     } else {
         return (
         <header className="white bg-blue w-100 sans-serif ma0 pa2 pl3 top-0 flex items-center">
+            <Link to={{pathname: `/channel`}} className='home-button white'><img src={home}/></Link>
             <h1 className='mr2'>{props.headerTitle}</h1>
-            <img src={props.headerImage} height='36px'></img>
-            <Link to={{pathname: `/channel`}} className='f5 no-underline ph3 pv2 mb2 dib tc br3 bn bg-animate bg-black-70 hover-bg-black white absolute right-0'>Home</Link>
         </header>
     )
     }
-}
+};
 export default Header;
