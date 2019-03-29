fdf99a9764139a2033006720b671621e234f636e
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-29 10:42:20 +0000

    Home button top right

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import header from '../css/header.css';
+import { Link } from 'react-router-dom'
 
 const Header = (props) => {
     if (props.headerImage == null) {
     return (
         <header className="gradient white bg-blue w-100 sans-serif ma0 pa2 pl3 top-0 flex justify-center">
             <h1 className='mr2'>{props.headerTitle}</h1>
+            <Link to={{pathname: `/channel`}} className='f5 no-underline ph3 pv2 mb2 dib tc br3 bn bg-animate bg-black-70 hover-bg-black white absolute right-0'>Home</Link>
         </header>
     )
     } else {
@@ -13,6 +15,7 @@ const Header = (props) => {
         <header className="white bg-blue w-100 sans-serif ma0 pa2 pl3 top-0 flex items-center">
             <h1 className='mr2'>{props.headerTitle}</h1>
             <img src={props.headerImage} height='36px'></img>
+            <Link to={{pathname: `/channel`}} className='f5 no-underline ph3 pv2 mb2 dib tc br3 bn bg-animate bg-black-70 hover-bg-black white absolute right-0'>Home</Link>
         </header>
     )
     }
