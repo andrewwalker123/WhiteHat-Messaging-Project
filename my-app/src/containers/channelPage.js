86d71dd94f27f23ef44ec07bf2cac5402b481b2a
Anna Burley <anna.burley@sky.uk>
2019-03-29 10:53:32 +0000

    Added styles for channel page

--- my-app/src/containers/channelPage.js
+++ my-app/src/containers/channelPage.js
@@ -3,20 +3,26 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Body from '../components/Body';
 import slugify from '../utils/slugify';
+import channelPage from '../css/channelPage.css'
 
 const ChannelPage = (props) => {
 
   const { channelName, channelIcon, programme: {contentUrl, title, desc} } = props.location.data.channel
   const slugUrl = slugify(channelName);
   return (
-    <div className="bg-washed-blue">
-      <Header headerTitle={channelName} headerImage={channelIcon}/>
+    <div>
+      <Header headerTitle={channelName}/>
       <Body>
-        <div>
-          <img src={contentUrl} height='50%' width='50%'/>
+        <div className="flex-container">
+          <div className="image-wrapper">
+            <img src={contentUrl} alt="show-thumbnail"/>
+          </div>
+          <div className="container">
+            <h1 className="white sans-serif">{title}</h1>
+            <h3 className="white tj">{desc}</h3>
+          </div>
+
         </div>
-        <h1>{title}</h1>
-        <h2>{desc}</h2>
       </Body>
       <Link to={{pathname: `/channel/${slugUrl}/chat`, data: props.location.data.channel}}>
         <button className="bg-green white pa3 w-100 sans-serif ma0 ba fixed" style={{bottom:0}}>JOIN CHAT</button>
